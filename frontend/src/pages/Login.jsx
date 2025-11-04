53876f59946cc3bc745d237d4467a1f1fa4bd1dc
JuanJostian <00142724@uca.edu.sv>
2025-11-04 14:09:26 -0600

    Actualizando login y añadiendo estilo :sparkles:

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,12 +1,15 @@
+import "../styles/login.css"
+
 export default function Login() {
   return (
-    <>
+    <div className="login-wrapper">
       <h1>Iniciar sesión</h1>
+      <p className="subtitle"> Bienvenido de vuelta!</p>
       <form>
         <input placeholder="Email" />
         <input placeholder="Password" type="password" />
         <button type="button">Entrar</button>
       </form>
-    </>
+    </div>
   );
 }
